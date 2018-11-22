527b757070a81201ea376005676d0e9a244556f8
christiandavidfs <christiandavid.fs@gmail.com>
2018-11-22 16:24:46 -0300

    Add files via upload
    Commented the code and small changes in the error messages.

--- pytest2.py
+++ pytest2.py
@@ -13,17 +13,23 @@ import json
         # # Let the base class default method raise the TypeError
         # return json.JSONEncoder.default(self, obj)
 
+# Here I open de csv with the company list and read the content line per line
 with open('companylist.csv', 'r') as f:
 	reader = csv.reader(f)
 	for row in f:
-	
+		# Define the base url
 		url= "http://stock-whatif.herokuapp.com/whatif/"
+		# Define a variable to the to the formed url
 		formedurl= url+row
+		# Get the status of the url 
 		statusurl = urllib.request.urlopen(url+row)	
+		# Get the content and load it as json format
 		contents = urllib.request.urlopen(formedurl).read()
 		d = json.loads(contents)
+		# Pass the status as string to validate it later
 		status= str(statusurl.getcode())
 		
+		# Get the Values for low and validate the types are correct.
 		def lowvalidation():
 			ledate=json_data.get("low", {}).get("date", {})
 			leprice=json_data.get("low", {}).get("price", {})
@@ -37,7 +43,7 @@ with open('companylist.csv', 'r') as f:
 				print ("All the data types for low are correct.")
 			else:
 				print ("Wrong data types for low.")
-				
+		# Get the Values for low and validate the types are correct.		
 		def highvalidation():
 			ledate2=json_data.get("high", {}).get("date", {})
 			leprice2=json_data.get("high", {}).get("price", {})
@@ -58,28 +64,36 @@ with open('companylist.csv', 'r') as f:
 				
 		response = json.dumps( d )
 		json_data = json.loads(response)
-		assert_valid_schema(json_data, 'schema.json')
+		# assert_valid_schema(json_data, 'schema.json')
+		# Print the content only for high
 		print ("High :"+ str(json_data['high']))
+		# Print the content only for low
 		print ("Low :"+ str(json_data['low']))
+		# Define two variables with the values of high and low
 		valuelow=json_data.get("low", {}).get("value", {})
 		valuehigh=json_data.get("high", {}).get("value", {})
 		print ()
+		# Prints the difference
 		print ("Values difference")
 		diferencia =valuehigh-valuelow
+		print (diferencia)
+		# Shows if the content for high and low have the right type of fields
 		lowvalidation()
 		highvalidation()
-		print (diferencia)
 		print ("---------------------------------------------------------")
+		# An evaluation to show the Higher value is reallly the higher
 		if valuehigh > valuelow:	
 			print ("Test Passed: Higher value is really higher")
 		else:
-			print ("Test Failed: Higher is in fact lower or equal")	
+			print ("Test Failed: The higher value is in fact lower or equal")	
 		print ("---------------------------------------------------------")
+		# Validates the Code Status response 
 		if status == "200":	
 			print ("Status 200 - Test Passed")
 		else:
 			print ("Status not 200 - Test Failed")	
 		print ("---------------------------------------------------------")
+		# Evaluates if the content in the fields is null
 		if "null" in str(contents):
 			print ('Failed. The response have null fields')
 		else:
